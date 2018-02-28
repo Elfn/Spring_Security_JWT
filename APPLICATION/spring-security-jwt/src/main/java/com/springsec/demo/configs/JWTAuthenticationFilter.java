a0f78576ac0961da0e2691324ac1a0d7b7bc8844
Elimane Fofana <Elimane.Fofana@supinfo.com>
2018-02-28 04:41:24 +0100

    enable security

--- APPLICATION/spring-security-jwt/src/main/java/com/springsec/demo/configs/JWTAuthenticationFilter.java
+++ APPLICATION/spring-security-jwt/src/main/java/com/springsec/demo/configs/JWTAuthenticationFilter.java
@@ -1,7 +1,15 @@
 package com.springsec.demo.configs;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by Elimane on Feb, 2018, at 05:56
@@ -11,5 +19,16 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
+    }
+
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }
--- APPLICATION/spring-security-jwt/src/main/java/com/springsec/demo/configs/SecurityConfig.java
+++ APPLICATION/spring-security-jwt/src/main/java/com/springsec/demo/configs/SecurityConfig.java
@@ -14,24 +14,34 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  * Created by Elimane on Feb, 2018, at 05:37
  */
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity//To activate web security
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//    @Autowired
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        //auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("1234").roles("ADMIN","USER")
+                .and()
+                .withUser("student").password("123").roles("USER");
+
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//To say to spring don't create httpSession
+//        http.csrf().disable();
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//To say to spring don't create httpSession
+
+        http.formLogin();
+        http.authorizeRequests().anyRequest().authenticated();
+
 
     }
 }
