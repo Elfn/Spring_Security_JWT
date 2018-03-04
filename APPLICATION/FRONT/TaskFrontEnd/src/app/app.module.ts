11d5c0dada445f8f98759df71a1143dca03eb418
Elimane Fofana <Elimane.Fofana@supinfo.com>
2018-03-04 04:31:03 +0100

    login form front angular 5

--- APPLICATION/FRONT/TaskFrontEnd/src/app/app.module.ts
+++ APPLICATION/FRONT/TaskFrontEnd/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes,RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { NewTaskComponent } from './components/new-task/new-task.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
+const appRoutes: Routes=[
+  {path:"login",component:LoginComponent},
+  {path:"tasks",component:TasksComponent},
+  {path:"new-task",component:NewTaskComponent},
+  {path:"register",component: RegistrationComponent},
+  {path:'',redirectTo:"login",pathMatch:"full"}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TasksComponent,
+    NewTaskComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes),FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
